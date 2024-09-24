25e19cb867138510bd71abbe8c4bc14d7dd7e99d
rowancherodian <63108571+rowancherodian@users.noreply.github.com>
2024-09-24 17:50:42 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -68,7 +68,7 @@ particlesJS('particles-js',
         "width": 1
       },
       "move": {
-        "enable": false,
+        "enable": true,
         "speed": 6,
         "direction": "none",
         "random": false,
